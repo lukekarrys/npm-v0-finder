4d062ed59d7a198d3ffb1b9578f9a4bda5a1eab1
Luke Karrys <lukekarrys@gmail.com>
2014-08-12 12:21:39 -0700

    domready

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var request = require('reqwest');
 var qs = require('qs');
 var _ = require('underscore');
 var async = require('async');
+var domready = require('domready');
 
 var user = (window.location.hash || '#lukekarrys').slice(1);
 var url = 'http://www.corsproxy.com/registry.npmjs.org/';
@@ -50,52 +51,54 @@ function isStable(obj) {
   return obj && obj.version && obj.version.slice(1) !== '0';
 }
 
-// Start
-appendP('Fetching modules');
+domready(function () {
+  // Start
+  appendP('Fetching modules');
 
-// Fetch all user modules
-request(userUrl + '?' + qs.stringify(params), function (respText) {
-  var resp;
-  var rows;
-  var modules;
-  
-  try {
-    resp = JSON.parse(respText);
-    rows = resp.rows || [];
-    appendP('Fetched ' + rows.length + ' modules');
-  } catch (e) {
-    appendP('Found no modules');
-    return;
-  }
-  
-  // Get names of all modules
-  modules = _.compact(rows.map(function (row) {
-    return row.key && row.key[1];
-  }));
-  
-  // Fetch all module versions
-  async.mapLimit(modules, 3, function (module, cb) {
-    appendS('.');
-    request({
-      url: url + module,
-      success: function (resp) {
-        cb(null, {
-          name: module,
-          version: resp['dist-tags'].latest
-        });
-      },
-      error: function () {
-        cb(null, new Error(module));
-      }
-     });
-  }, function (err, results) {
-    var errors = _.filter(results, isError);
-    var dividedModules = _.partition(_.reject(results, isError), isStable);
-    //var v1 = dividedModules[0] || [];
-    var v0 = dividedModules[1] || [];
+  // Fetch all user modules
+  request(userUrl + '?' + qs.stringify(params), function (respText) {
+    var resp;
+    var rows;
+    var modules;
+    
+    try {
+      resp = JSON.parse(respText);
+      rows = resp.rows || [];
+      appendP('Fetched ' + rows.length + ' modules');
+    } catch (e) {
+      appendP('Found no modules');
+      return;
+    }
+    
+    // Get names of all modules
+    modules = _.compact(rows.map(function (row) {
+      return row.key && row.key[1];
+    }));
+    
+    // Fetch all module versions
+    async.mapLimit(modules, 3, function (module, cb) {
+      appendS('.');
+      request({
+        url: url + module,
+        success: function (resp) {
+          cb(null, {
+            name: module,
+            version: resp['dist-tags'].latest
+          });
+        },
+        error: function () {
+          cb(null, new Error(module));
+        }
+       });
+    }, function (err, results) {
+      var errors = _.filter(results, isError);
+      var dividedModules = _.partition(_.reject(results, isError), isStable);
+      //var v1 = dividedModules[0] || [];
+      var v0 = dividedModules[1] || [];
 
-    displayModules('0.x.y', v0);
-    // displayModules('>=1.x.y', v1);
-    displayModules('Could not find', _.pluck(errors, 'message'));
+      displayModules('0.x.y', v0);
+      // displayModules('>=1.x.y', v1);
+      displayModules('Could not find', _.pluck(errors, 'message'));
+    });
   });
 });
