d8b45578cec9ae7288f9e5360026113706ee662d
Luke Karrys <lukekarrys@gmail.com>
2016-12-07 18:29:28 -0700

    page.html

--- config.js
+++ config.js
@@ -4,6 +4,9 @@ module.exports = function () {
             main: __dirname + '/app.js',
             resourcePrefix: './'
         },
+        htmlSource: function (context) {
+          return require('fs').readFileSync('./page.html').toString().replace('{{entry}}', context.resourcePrefix + context.jsFileName);
+        },
         directory: __dirname + '/_dist',
     };
 };
